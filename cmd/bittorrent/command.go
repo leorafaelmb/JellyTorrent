fac948afd00acedc533c6c60f9bb723454897128
leorafaelmb <leorafaelmb@gmail.com>
2026-03-09 14:42:27 -0400

    Integrate DHT for peer discovery into client

--- cmd/bittorrent/command.go
+++ cmd/bittorrent/command.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"context"
 	"crypto/rand"
 	"path/filepath"
+	"time"
 
+	Kademlia "github.com/leorafaelmb/Kademlia"
 	"github.com/leorafaelmb/BitTorrent-Client/internal/downloader"
 	"github.com/leorafaelmb/BitTorrent-Client/internal/logger"
 	"github.com/leorafaelmb/BitTorrent-Client/internal/metainfo"
@@ -22,6 +25,74 @@ func runCommand(command string, args []string) error {
 	}
 	return nil
 
+}
+
+// startDHT creates and bootstraps a DHT node. Returns the DHT instance
+// (caller must Close it) or nil if bootstrap fails.
+func startDHT() *Kademlia.DHT {
+	dht, err := Kademlia.New()
+	if err != nil {
+		logger.Log.Debug("failed to create DHT node", "error", err)
+		return nil
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := dht.Bootstrap(ctx); err != nil {
+		logger.Log.Debug("DHT bootstrap failed", "error", err)
+		dht.Close()
+		return nil
+	}
+
+	logger.Log.Info("DHT bootstrapped")
+	return dht
+}
+
+// dhtPeers queries the DHT for peers and returns them merged with existing peers.
+func dhtPeers(dht *Kademlia.DHT, infoHash [20]byte, existing []peer.Peer) []peer.Peer {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	addrs, err := dht.GetPeers(ctx, infoHash)
+	if err != nil {
+		logger.Log.Debug("DHT get_peers failed", "error", err)
+		return existing
+	}
+
+	// Deduplicate against existing peers
+	seen := make(map[string]bool, len(existing))
+	for _, p := range existing {
+		seen[p.AddrPort.String()] = true
+	}
+
+	added := 0
+	for _, addr := range addrs {
+		key := addr.String()
+		if !seen[key] {
+			existing = append(existing, peer.Peer{AddrPort: addr})
+			seen[key] = true
+			added++
+		}
+	}
+
+	if added > 0 {
+		logger.Log.Info("DHT peers discovered", "new", added, "total", len(existing))
+	}
+
+	return existing
+}
+
+// dhtAnnounce announces to the DHT that we are downloading/seeding a torrent.
+func dhtAnnounce(dht *Kademlia.DHT, infoHash [20]byte, port int) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := dht.Announce(ctx, infoHash, port); err != nil {
+		logger.Log.Debug("DHT announce failed", "error", err)
+	} else {
+		logger.Log.Debug("DHT announce complete")
+	}
 }
 
 func handleDownload(args []string) error {
@@ -65,13 +136,21 @@ func handleDownload(args []string) error {
 		return err
 	}
 
-	logger.Log.Info("peers discovered", "count", len(trackerResp.Peers))
+	logger.Log.Info("tracker peers discovered", "count", len(trackerResp.Peers))
 
-	// Create Peer objects from addresses
+	// Create Peer objects from tracker addresses
 	peerList := make([]peer.Peer, len(trackerResp.Peers))
 	for i, addr := range trackerResp.Peers {
 		addrCopy := addr
 		peerList[i] = peer.Peer{AddrPort: addrCopy}
+	}
+
+	// DHT: bootstrap, discover additional peers, announce
+	dht := startDHT()
+	if dht != nil {
+		defer dht.Close()
+		peerList = dhtPeers(dht, t.Info.InfoHash, peerList)
+		go dhtAnnounce(dht, t.Info.InfoHash, 6881)
 	}
 
 	if err = downloader.DownloadFile(t, peerList, 50, downloadFilePath,
@@ -138,11 +217,19 @@ func handleMagnetDownload(args []string) error {
 		return err
 	}
 
-	logger.Log.Info("peers discovered", "count", len(annResp.Peers))
+	logger.Log.Info("tracker peers discovered", "count", len(annResp.Peers))
 
 	peerList := make([]peer.Peer, len(annResp.Peers))
 	for i, addr := range annResp.Peers {
 		peerList[i] = peer.Peer{AddrPort: addr}
+	}
+
+	// DHT: bootstrap, discover additional peers, announce
+	dht := startDHT()
+	if dht != nil {
+		defer dht.Close()
+		peerList = dhtPeers(dht, magnet.InfoHash, peerList)
+		go dhtAnnounce(dht, magnet.InfoHash, 6881)
 	}
 
 	if err = downloader.DownloadFile(&t, peerList, 50, downloadFilePath,
